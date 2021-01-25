f3488e92babc635680c21ed3e193f9a84ef885be
Kyle <kmcdaniel@ehe.health>
2021-01-25 11:53:05 -0500

    Add datepicker

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import React, { useState } from 'react';
+import { DatePicker, MuiPickersUtilsProvider} from '@material-ui/pickers';
+import MomentUtils from '@date-io/moment';
 
 function App() {
   let image = "https://picsum.photos/200/300"
+  const [selectedDate, handleDateChange] = useState(new Date());
   return (
     <div className="App">
       <header className="App-header">
+      <MuiPickersUtilsProvider utils={MomentUtils}>
+        <DatePicker value={selectedDate} onChange={handleDateChange} />
+      </MuiPickersUtilsProvider>
         <p>
           <img src={image}></img>
         </p>
